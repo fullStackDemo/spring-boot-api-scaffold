44cdb13e474ce99157299985caebdbdd0c1ed0c5
alex wong <master2011zhao@gmail.com>
2020-06-15 18:04:48 +0800

    发送邮件------模板

--- src/main/java/com/scaffold/test/controller/MailController.java
+++ src/main/java/com/scaffold/test/controller/MailController.java
@@ -1,13 +1,17 @@
 package com.scaffold.test.controller;
 
+import ch.qos.logback.classic.pattern.EnsureExceptionHandling;
 import com.scaffold.test.base.Result;
 import com.scaffold.test.base.ResultGenerator;
 import com.scaffold.test.entity.Mail;
 import com.scaffold.test.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
 
@@ -19,6 +23,7 @@ public class MailController {
     private MailService mailService;
 
     // 发送不带格式的文本
+    @Async
     @GetMapping("post")
     public Result postMail() {
         Mail mail = new Mail();
@@ -30,6 +35,7 @@ public class MailController {
     }
 
     // 发送Html邮件
+    @Async
     @GetMapping("postHtml")
     public Result postHtmlMail() throws MessagingException {
         String content = "<html>\n" +
@@ -46,6 +52,7 @@ public class MailController {
     }
 
     // 发送带附件的邮件
+    @Async
     @GetMapping("postAttachment")
     public Result postAttachmentsMail() throws MessagingException {
         Mail mail = new Mail();
@@ -56,4 +63,22 @@ public class MailController {
         mailService.sendAttachmentsMail(mail);
         return ResultGenerator.getSuccessResult().setMessage("发送成功");
     }
+
+    // 发送 Html 模板邮件
+    @Async
+    @GetMapping("postTemplate")
+    public Result postTemplateMail() throws MessagingException {
+        Context context = new Context();
+        TemplateEngine templateEngine = new TemplateEngine();
+        context.setVariable("name", "alex wong");
+        String emailTemplate = templateEngine.process("emailTemplate", context);
+
+        Mail mail = new Mail();
+        mail.setTo("1498097245@qq.com");
+        mail.setSubject("模板邮件");
+        mail.setContent(emailTemplate);
+        mailService.sendHtmlMail(mail);
+        return ResultGenerator.getSuccessResult().setMessage("发送成功");
+    }
+
 }
