edd5b4b826502662b98143f3d83d083bd62f3717
master <master2011zhao@gmail.com>
2020-07-04 10:32:42 +0800

    定时任务开启配置

--- src/main/java/com/scaffold/test/ScaffoldApplication.java
+++ src/main/java/com/scaffold/test/ScaffoldApplication.java
@@ -4,13 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @MapperScan("com.scaffold.test.mapper")
 @RestController
-@EnableScheduling
+//@EnableScheduling
 @EnableAsync
 public class ScaffoldApplication {
 
