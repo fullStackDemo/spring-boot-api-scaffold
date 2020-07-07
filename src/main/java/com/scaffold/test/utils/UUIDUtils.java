fad7131f1e35b385811be17b2a6e4e8f2cf63cc3
alex wong <master2011zhao@gmail.com>
2020-07-07 18:05:56 +0800

    加密

--- src/main/java/com/scaffold/test/utils/UUIDUtils.java
+++ src/main/java/com/scaffold/test/utils/UUIDUtils.java
@@ -1,7 +1,10 @@
 package com.scaffold.test.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.UUID;
 
+@Slf4j
 public class UUIDUtils {
 
     public static String getUUID(){
--- src/test/java/com/scaffold/test/ScaffoldApplicationTests.java
+++ src/test/java/com/scaffold/test/ScaffoldApplicationTests.java
@@ -1,23 +1,43 @@
 package com.scaffold.test;
 
-import com.scaffold.test.entity.User;
-import com.scaffold.test.mapper.UserMapper;
-import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ScaffoldApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ScaffoldApplicationTests {
 
-@SpringBootTest
-class ScaffoldApplicationTests {
+
 
     @Autowired
-    private UserMapper userMapper;
+    StringEncryptor encryptor;
 
-    @Test
-    void contextLoads() {
-        List<User> users = userMapper.selectList(null);
-        System.out.println(users);
-    }
+//    @Value("${spring.datasource.url}")
+//    String dataSourceUrl;
+//
+//    @Value("${spring.datasource.username}")
+//    String dataSourceName;
+//
+//    @Value("${spring.datasource.password}")
+//    String dataSourcePassword;
+//
+//    @Test
+//    public void getPass() {
+//        String url = encryptor.encrypt(dataSourceUrl);
+//        String name = encryptor.encrypt(dataSourceName);
+//        String password = encryptor.encrypt(dataSourcePassword);
+//        log.info("---url---");
+//        log.info(url);
+//        log.info("---name---");
+//        log.info(name);
+//        log.info("---password---");
+//        log.info(password);
+//    }
 
 }
