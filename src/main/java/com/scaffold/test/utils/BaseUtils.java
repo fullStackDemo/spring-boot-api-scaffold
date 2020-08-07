0751e017251d242db8d8a9bfad602933798d770a
alex wong <master2011zhao@gmail.com>
2020-08-07 14:26:00 +0800

    add

--- src/main/java/com/scaffold/test/utils/BaseUtils.java
+++ src/main/java/com/scaffold/test/utils/BaseUtils.java
@@ -69,7 +69,7 @@ public class BaseUtils {
         if (user == null) {
             return null;
         }
-        return getCurrentUser().getUserId();
+        return user.getUserId();
     }
 
 }
