7f4d5be5c2d5ec8ff33c60a93497712ebe0e1b98
alex wong <master2011zhao@gmail.com>
2020-09-24 09:41:51 +0800

    test

--- src/main/java/com/scaffold/test/fix/FixMock.java
+++ src/main/java/com/scaffold/test/fix/FixMock.java
@@ -1,7 +1,6 @@
 package com.scaffold.test.fix;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import quickfix.Session;
 import quickfix.SessionID;
@@ -20,12 +19,12 @@ public class FixMock {
     @Resource
     private SocketAcceptor socketAcceptor;
 
-    @Scheduled(cron = "0/10 * * * * ?")
+//    @Scheduled(cron = "0/10 * * * * ?")
     public void executeReport() throws Exception {
         if (Objects.isNull(socketAcceptor)) {
             return;
         }
-//        log.error("mock推送......");
+        log.info("mock推送......");
         ExecutionReport report = new ExecutionReport();
         report.set(new OrderID("22554912"));
         report.set(new ExecID("22554912"));
