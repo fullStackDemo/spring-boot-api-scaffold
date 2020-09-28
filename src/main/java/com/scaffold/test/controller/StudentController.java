88c89106c23a6990385033fa3ba3bc784a473f80
alex wong <master2011zhao@gmail.com>
2020-09-28 19:54:29 +0800

    add

--- src/main/java/com/scaffold/test/controller/StudentController.java
+++ src/main/java/com/scaffold/test/controller/StudentController.java
@@ -6,13 +6,12 @@ import com.scaffold.test.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -38,15 +37,15 @@ public class StudentController {
 
     @GetMapping("add")
 //    public void addStudent(Student student){
-    public String addStudent(@Valid Student student, BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
-            if(bindingResult.hasErrors()){
-                for (ObjectError error: bindingResult.getAllErrors()) {
-                    log.error(error.getDefaultMessage());
-                    return error.getDefaultMessage();
-                }
-            }
-        }
+    public String addStudent(@Validated Student student, BindingResult bindingResult){
+//        if(bindingResult.hasErrors()){
+//            if(bindingResult.hasErrors()){
+//                for (ObjectError error: bindingResult.getAllErrors()) {
+//                    log.error(error.getDefaultMessage());
+//                    return error.getDefaultMessage();
+//                }
+//            }
+//        }
         return studentService.saveStudent(student);
     }
 
--- src/main/java/com/scaffold/test/entity/Student.java
+++ src/main/java/com/scaffold/test/entity/Student.java
@@ -2,8 +2,8 @@ package com.scaffold.test.entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -21,7 +21,7 @@ public class Student implements Serializable {
 
     private static final long serialVersionUID=1L;
 
-    @Min(value = 1, message = "id不能为空")
+    @Range(min = 1, message = "id不能为空")
     private int id;
 
     @NotBlank(message = "name不能为空")
